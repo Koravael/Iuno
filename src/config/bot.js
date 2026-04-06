9f79a660642512c88bf9bfd24707ad5206a4f715
Koravael <jeremyacastro52@gmail.com>
2026-04-06 14:23:19 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "your gacha news",
+        name: "your gacha news | W.I.P",
         // Activity type number (0 = Playing).
         type: 1, 
       },
