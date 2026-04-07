03be9e7e28bab202dfe6468ad539c8a0b68f3448
Koravael <jeremyacastro52@gmail.com>
2026-04-06 21:00:28 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "ping @Koravael if offline!!",
+        name: "ping @Koravael if offline~",
         // Activity type number (0 = Playing).
-        type: 1, 
+        type: 3, 
       },
     ],
   },
